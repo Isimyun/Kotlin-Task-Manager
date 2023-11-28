b8a8f0d00832370d696099bb69de6e5d699962fe
Immanuel <immanuelsy2112@gmail.com>
2023-11-28 23:29:13 +0000

    Implemented profile fragment.

--- app/src/main/java/com/mobiledevelopment/task_manager/fragments/ProfileFragment.kt
+++ app/src/main/java/com/mobiledevelopment/task_manager/fragments/ProfileFragment.kt
@@ -1,60 +1,74 @@
 package com.mobiledevelopment.task_manager.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mobiledevelopment.task_manager.R
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.mobiledevelopment.task_manager.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        displayUserData()
+        displayTaskCount()
+    }
+
+    private fun init() {
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference.child("Tasks")
+    }
+
+    private fun displayUserData() {
+        val currentUser = auth.currentUser
+        currentUser?.let {
+            // Display user email
+            binding.userName.text = it.email
+        }
+    }
+
+    private fun displayTaskCount() {
+        val currentUser = auth.currentUser
+        currentUser?.let {
+            // Get the user's UID
+            val userId = it.uid
+
+            // Query tasks for the current user
+            database.child(userId)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        // Get the count of tasks
+                        val taskCount = snapshot.childrenCount.toInt()
+
+                        // Display the task count
+                        binding.password.text = "$taskCount TASKs PENDING"
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        // Handle the failure to retrieve tasks
+                        // You can add proper error handling here
+                    }
+                })
+        }
     }
 }
